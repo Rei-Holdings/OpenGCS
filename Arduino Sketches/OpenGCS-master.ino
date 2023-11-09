b1617940bcfaf171b6614a285089e527da9bb498
Rei Johnson <reirodneyjohnson@gmail.com>
2023-11-08 23:05:24 -0500

    Added IncludePath edits and some code

--- Arduino Sketches/OpenGCS-master.ino
+++ Arduino Sketches/OpenGCS-master.ino
@@ -1,8 +1,12 @@
+#include <time.h>
+#include <stdio.h>
+#include <Arduino.h>
 
 void setup() {
-    
+    Serial.begin(115400);
 }
 
 void loop() {
-
+    // Save data to file for Processing
+    
 }
---
+++ ProcessingUI/ProcessingUI.pde
@@ -0,0 +1,5 @@
+void setup(){
+    size(1080, 720);
+    noStroke();
+    background(0);
+}
